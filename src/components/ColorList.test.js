f0fa42a6cad336c7e056733d6db7adee1113f650
Rick <ricksmycodeguy@gmail.com>
2021-09-17 14:09:38 -0500

    ColorList.test.js all tests passing

--- src/components/ColorList.test.js
+++ src/components/ColorList.test.js
@@ -1,14 +1,40 @@
 import React from 'react';
-import MutationObserver from 'mutationobserver-shim';
-
+// import MutationObserver from 'mutationobserver-shim';
 import { render, screen} from "@testing-library/react";
 import ColorList from './ColorList';
 
+const testColors = [
+    {code: {hex: "##ffebcd"},
+    color: "blanchedalmond",
+    id: 9},
+    {code: {hex: "#6093ca"},
+    color: "blue",
+    id: 10},
+    {code: {hex: "#8a2be2"},
+    color: "blueviolet",
+    id: 11}
+];
 test("Renders an empty list of colors without errors", () => {
+    render(<ColorList colors={[]} />)
 });
 
 test("Renders a list of colors without errors", () => {
+    render(<ColorList colors={testColors} />)
 });
 
 test("Renders the EditForm when editing = true and does not render EditForm when editing = false", () => {
+    const { rerender } = render(<ColorList colors={testColors} editing={true} />)
+    const editColorLegend = screen.getByText(/edit color/i);
+    const labelForColorName = screen.getByText(/color name/i);
+    const labelForColorHex = screen.getByText(/hex code/i);
+
+    expect(editColorLegend).toBeInTheDocument()
+    expect(labelForColorName).toBeInTheDocument();
+    expect(labelForColorHex ).toBeInTheDocument();
+
+    rerender(<ColorList colors={testColors} editing={false} />);
+    
+    expect(editColorLegend).not.toBeInTheDocument();
+    expect(labelForColorName).not.toBeInTheDocument();
+    expect(labelForColorHex).not.toBeInTheDocument();
 });
