4b576f94a9601e98bfd9c037012701137f7c6520
Rick <ricksmycodeguy@gmail.com>
2021-09-17 12:24:28 -0500

    Add axiosWithAuth.js  & start fetchColorService.js

--- src/services/fetchColorService.js
+++ src/services/fetchColorService.js
@@ -1,6 +1,15 @@
 import axiosWithAuth from '../helpers/axiosWithAuth';
 
-const fetchColorService = () => {
+const fetchColorService = (setColor) => {
+    console.log('fetchColorService.js ln:4 setColor', setColor);
+    axiosWithAuth.get('http://localhost:5000/api/colors')
+    .then(res=>{
+        console.log('');
+
+    })
+    .catch(err =>{
+        console.log('error');
+    })
     
 }
 
